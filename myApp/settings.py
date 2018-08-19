bea37a48a94ad255f81233c2bbdb8c091af3fa2f
scott <scott.lan@taisys.com>
2018-08-19 22:13:26 +0800

    update code

--- myApp/settings.py
+++ myApp/settings.py
@@ -75,6 +75,10 @@ WSGI_APPLICATION = 'myApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myApp',
@@ -82,9 +86,7 @@ DATABASES = {
         # 'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
